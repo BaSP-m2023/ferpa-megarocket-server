f8ad4b24607164723b6e8287242c611ebc26d179
Eugenia Gassman <eugeniagassman@gmail.com>
2023-05-07 21:04:02 -0300

    MR:added put method on members

--- src/resources/member.js
+++ src/resources/member.js
@@ -24,4 +24,38 @@ router.delete('/:id', (req, res) => {
   }
 });
 
+router.put('/:id', (req, res) => {
+  const memberId = req.params.id;
+  const foundMember = members.find((member) => member.id.toString() === memberId);
+  if (foundMember) {
+    const updMember = req.body;
+    members.forEach((member) => {
+      if (member.id.toString() === memberId) {
+        const member2 = member;
+        member2.membership = updMember.membership ? updMember.membership : member2.membership;
+        member2.firstName = updMember.firstName ? updMember.firstName : member2.firstName;
+        member2.lastName = updMember.lastName ? updMember.lastName : member2.lastName;
+        member2.dob = updMember.dob ? updMember.dob : member2.dob;
+        member2.phone = updMember.phone ? updMember.phone : member2.phone;
+        member2.address = updMember.address ? updMember.address : member2.address;
+        member2.city = updMember.city ? updMember.city : member2.city;
+        member2.zip = updMember.zip ? updMember.zip : member2.zip;
+        member2.email = updMember.email ? updMember.email : member2.email;
+        member2.password = updMember.password ? updMember.password : member2.password;
+        member2.dni = updMember.dni ? updMember.dni : member2.dni;
+
+        fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
+          if (err) {
+            res.send('Error! Cant update');
+          } else {
+            res.send('Member updated');
+          }
+        });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
 module.exports = router;
