e884f52de06bb7b6ba083588671daf359916ed92
Victoria Negrotto <vnegrotto19@gmail.com>
2023-05-08 20:34:07 -0300

    MR: fixed the member.js

--- src/resources/member.js
+++ src/resources/member.js
@@ -6,36 +6,36 @@ const members = require('../data/member.json');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send(members);
+  res.status(200).json({ members });
 });
 
 router.get('/:id', (req, res) => {
   const memberId = req.params.id;
   const foundMember = members.find((member) => member.id.toString() === memberId);
   if (foundMember) {
-    res.send(foundMember);
+    res.status(200).json({ foundMember });
   } else {
-    res.send('Member not found');
+    res.status(400).json({ msg: 'Member not found' });
   }
 });
 
 router.post('/post', (req, res) => {
   const newMember = req.body;
   if (!newMember.id) {
-    res.send('Error! Invalid member ID');
+    res.status(400).json({ msg: 'Error! Invalid member ID' });
   } else {
     const existingMember = members.find((member) => member.id === newMember.id);
     if (!existingMember) {
       members.push(newMember);
       fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
         if (err) {
-          res.send('Error! Member cannot be created');
+          res.status(400).json({ msg: 'Error! Member cannot be created' });
         } else {
-          res.send('Member created!');
+          res.status(200).json({ msg: 'Member created!' });
         }
       });
     } else {
-      res.send('Error! Member with this ID already exists');
+      res.status(400).json({ msg: 'Error! Member with this ID already exists' });
     }
   }
 });
