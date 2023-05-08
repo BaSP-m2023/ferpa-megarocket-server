a8da4bda189004557cb09c17ff19cb914ceed234
Victoria Negrotto <vnegrotto19@gmail.com>
2023-05-07 22:09:44 -0300

    MR: worked on Post of Members

--- src/resources/member.js
+++ src/resources/member.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const members = require('../data/member.json');
 
@@ -15,6 +16,27 @@ router.get('/:id', (req, res) => {
     res.send(foundMember);
   } else {
     res.send('Member not found');
+  }
+});
+
+router.post('/post', (req, res) => {
+  const newMember = req.body;
+  if (!newMember.id) {
+    res.send('Error! Invalid member ID');
+  } else {
+    const existingMember = members.find((member) => member.id === newMember.id);
+    if (!existingMember) {
+      members.push(newMember);
+      fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
+        if (err) {
+          res.send('Error! Member cannot be created');
+        } else {
+          res.send('Member created!');
+        }
+      });
+    } else {
+      res.send('Error! Member with this ID already exists');
+    }
   }
 });
 
