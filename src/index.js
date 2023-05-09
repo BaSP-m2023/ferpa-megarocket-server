15505bd2ff45ff3ef7538d6d92b02a6ca012a3b5
Juan Ignacio Canton <juanignaciocanton1@gmail.com>
2023-05-08 22:35:19 -0300

    MR: fixes in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // use "import" to import libraries
 import express from 'express';
 import cors from 'cors';
-
 // use "require" to import JSON files
-// const admins = require('./data/admins.json');
+const admins = require('./data/admins.json');
 
 const adminsRouter = require('./resources/admins');
 
@@ -15,16 +14,14 @@ app.use(express.json());
 
 app.use('/admins', adminsRouter);
 
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
-}); */
-
-/* app.get('/admins', (req, res) => {
+});
+app.get('/admins', (req, res) => {
   res.status(200).json({
     data: admins,
-
-}); */
-
+  });
+});
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
