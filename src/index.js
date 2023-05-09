b3f14d1e935a2f6e1a38562bf953cf2dbdac207c
leo-balonchard <leobalonchard@gmail.com>
2023-05-09 17:46:01 -0300

    MR: Put and delete for admins

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-// use "import" to import libraries
 import express from 'express';
 import cors from 'cors';
 
-// use "require" to import JSON files
 const admins = require('./data/admins.json');
+const adminsRouter = require('./resources/admins');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/admins', adminsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
