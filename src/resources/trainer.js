9f4bedfff1cb3a3fe61a7c34901a6e97b9f211ef
matiasluispeter <matiasluispeter@gmail.com>
2023-05-10 15:27:45 -0300

    MR: add const new trainer, remove path /post and remove else in found trainer

--- src/resources/trainer.js
+++ src/resources/trainer.js
@@ -4,13 +4,13 @@ const trainers = require('../data/trainer.json');
 
 const router = express.Router();
 
-router.get('/get', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json({
     data: trainers,
   });
 });
 
-router.get('/getById/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const trainerId = req.params.id;
   const foundTrainer = trainers.find((trainer) => trainer.id.toString() === trainerId);
   if (foundTrainer) {
@@ -23,23 +23,29 @@ router.get('/getById/:id', (req, res) => {
   }
 });
 
-router.post('/post', (req, res) => {
-  const newTrainer = req.body;
-  const newTrainerId = parseInt(req.body.id, 10);
-  const foundTrainer = trainers.find((trainer) => trainer.id === newTrainerId);
-  if (!newTrainer.id || !newTrainer.firstName || !newTrainer.lastName
-     || !newTrainer.phone || !newTrainer.activityId || !newTrainer.salary
-     || !newTrainer.status) {
+router.post('/', (req, res) => {
+  const oldTrainer = req.body;
+  if (!oldTrainer.id || !oldTrainer.firstName || !oldTrainer.lastName
+     || !oldTrainer.phone || !oldTrainer.activityId || !oldTrainer.salary
+     || !oldTrainer.status) {
     res.status(400).json({ msg: 'We need an id, trainer name, trainer surname, phone, activityId, salary and status' });
-  } else if (foundTrainer) {
-    res.status(400).json({ msg: 'Trainer already exists!' });
   } else {
+    const trainerIdentification = trainers[trainers.length - 1].id + 1;
+    const newTrainer = {
+      id: trainerIdentification,
+      firstName: oldTrainer.firstName,
+      lastName: oldTrainer.lastName,
+      phone: oldTrainer.phone,
+      activityId: oldTrainer.activityId,
+      salary: oldTrainer.salary,
+      status: oldTrainer.status,
+    };
     trainers.push(newTrainer);
     fs.writeFile('src/data/trainer.json', JSON.stringify(trainers, null, 2), (err) => {
       if (err) {
         res.status(400).json({ msg: 'Error! Trainer cant be created' });
       } else {
-        res.status(200).json({ msg: 'Trainer created!' });
+        res.status(200).json({ msg: 'Trainer created!', data: newTrainer });
       }
     });
   }
