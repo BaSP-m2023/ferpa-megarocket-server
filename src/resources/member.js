2d2b5ac00c2008fda7d4acae98af28628fa3b1f2
Victoria Negrotto <vnegrotto19@gmail.com>
2023-05-10 15:58:25 -0300

    fixed package-lock.json and made correction of get and post methods

--- src/resources/member.js
+++ src/resources/member.js
@@ -6,37 +6,46 @@ const members = require('../data/member.json');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json({ members });
+  res.status(200).json({ data: members });
 });
 
 router.get('/:id', (req, res) => {
   const memberId = req.params.id;
   const foundMember = members.find((member) => member.id.toString() === memberId);
   if (foundMember) {
-    res.status(200).json({ foundMember });
+    res.status(200).json({ data: foundMember });
   } else {
     res.status(400).json({ msg: 'Member not found' });
   }
 });
 
 router.post('/', (req, res) => {
-  const newMember = req.body;
-  if (!newMember.id) {
-    res.status(400).json({ msg: 'Error! Invalid member ID' });
+  const oldMember = req.body;
+  if (!oldMember.firstName || !oldMember.lastName
+    || !oldMember.email || !oldMember.password || !oldMember.dni) {
+    res.status(400).json({ msg: 'Error! Please complete all the filds' });
   } else {
-    const existingMember = members.find((member) => member.id === newMember.id);
-    if (!existingMember) {
-      members.push(newMember);
-      fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
-        if (err) {
-          res.status(400).json({ msg: 'Error! Member cannot be created' });
-        } else {
-          res.status(200).json({ msg: 'Member created!' });
-        }
-      });
-    } else {
-      res.status(400).json({ msg: 'Error! Member with this ID already exists' });
-    }
+    const memberId = members[members.length - 1].id + 1;
+    const newMember = {
+      id: memberId,
+      firstName: oldMember.firstName,
+      lastName: oldMember.lastName,
+      dob: oldMember.dob,
+      phone: oldMember.phone,
+      address: oldMember.address,
+      city: oldMember.city,
+      zip: oldMember.zip,
+      email: oldMember.email,
+      password: oldMember.password,
+      dni: oldMember.dni,
+    };
+    members.push(newMember);
+    fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
+      if (err) {
+        res.status(400).json({ msg: 'Error! Member cannot be created' });
+      }
+      res.status(200).json({ msg: 'Member created!', data: newMember });
+    });
   }
 });
 
