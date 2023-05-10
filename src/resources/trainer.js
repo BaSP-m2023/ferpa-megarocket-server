3a1d12bda0cc96238322b7f0bc7806c5a576405e
Galo Durante <durantegalo@gmail.com>
2023-05-10 12:33:33 -0300

    MR: correct the code and the package-lock.json

--- src/resources/trainer.js
+++ src/resources/trainer.js
@@ -5,28 +5,26 @@ const trainers = require('../data/trainer.json');
 
 const router = express.Router();
 
-router.put('/put/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const trainerId = req.params.id;
   const updateTrainer = req.body;
   const foundTrainer = trainers.find((trainer) => trainer.id.toString() === trainerId);
   if (foundTrainer) {
-    trainers.forEach((trainer) => {
-      if (trainer.id.toString() === trainerId) {
-        const aux = trainer;
-        aux.firstName = updateTrainer.firstName ? updateTrainer.firstName : aux.firstName;
-        aux.lastName = updateTrainer.lastName ? updateTrainer.lastName : aux.lastName;
-        aux.phone = updateTrainer.phone ? updateTrainer.phone : aux.phone;
-        aux.activityId = updateTrainer.activityId ? updateTrainer.activityId : aux.activityId;
-        aux.salary = updateTrainer.salary ? updateTrainer.salary : aux.salary;
-        aux.status = updateTrainer.status ? updateTrainer.status : aux.status;
-
-        fs.writeFile('src/data/trainer.json', JSON.stringify(trainers, null, 2), (err) => {
-          if (err) {
-            res.status(400).json({ msg: 'Error! Trainer can not be update' });
-          } else {
-            res.status(200).json({ msg: 'Trainer updated!' });
-          }
-        });
+    foundTrainer.firstName = updateTrainer.firstName ? updateTrainer.firstName
+      : foundTrainer.firstName;
+    foundTrainer.lastName = updateTrainer.lastName ? updateTrainer.lastName : foundTrainer.lastName;
+    foundTrainer.phone = updateTrainer.phone ? updateTrainer.phone : foundTrainer.phone;
+    foundTrainer.activityId = updateTrainer.activityId ? updateTrainer.activityId
+      : foundTrainer.activityId;
+    foundTrainer.salary = updateTrainer.salary ? updateTrainer.salary : foundTrainer.salary;
+    foundTrainer.status = updateTrainer.status ? updateTrainer.status : foundTrainer.status;
+    const trainer = trainers.filter((train) => train.id.toString() !== trainerId);
+    trainer.push(foundTrainer);
+    fs.writeFile('src/data/trainer.json', JSON.stringify(trainers, null, 2), (err) => {
+      if (err) {
+        res.status(400).json({ msg: 'Error! Trainer can not be update' });
+      } else {
+        res.status(200).json({ msg: 'Trainer updated!', data: foundTrainer });
       }
     });
   } else {
@@ -34,7 +32,7 @@ router.put('/put/:id', (req, res) => {
   }
 });
 
-router.delete('/delete/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const trainerId = req.params.id;
   const filterTrainer = trainers.filter((trainer) => trainer.id.toString() !== trainerId);
   if (filterTrainer.length === trainers.length) {
