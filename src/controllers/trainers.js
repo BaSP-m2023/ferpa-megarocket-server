5a7b469f6b548a9e2c80ad5f43b8201c1b4771a9
LucianoStradiot <lstradiot15@gmail.com>
2023-05-13 19:05:01 -0300

    MR-3: create initial setup for trainers and add get all

--- src/controllers/trainers.js
+++ src/controllers/trainers.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const fs = require('fs');
-const trainers = require('../data/trainer.json');
+const Trainer = require('../models/Trainer');
 
-const router = express.Router();
+const getAllTrainers = (req, res) => {
+  Trainer.find()
+    .then((trainers) => res.status(200).json({
+      message: 'Trainers list',
+      data: trainers,
+      error: false,
+    }))
+    .catch((error) => res.status(500).json({
+      message: 'An ERROR ocurred',
+      error,
+    }));
+};
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    data: trainers,
-  });
-});
-
-router.get('/:id', (req, res) => {
+/* router.get('/:id', (req, res) => {
   const trainerId = req.params.id;
   const foundTrainer = trainers.find((trainer) => trainer.id.toString() === trainerId);
   if (foundTrainer) {
@@ -21,14 +24,15 @@ router.get('/:id', (req, res) => {
   } else {
     res.status(400).json({ msg: `No trainer with id ${trainerId} was found.` });
   }
-});
+}); */
 
-router.post('/', (req, res) => {
+/* router.post('/', (req, res) => {
   const oldTrainer = req.body;
   if (!oldTrainer.firstName || !oldTrainer.lastName
      || !oldTrainer.phone || !oldTrainer.activityId || !oldTrainer.salary
      || !oldTrainer.status) {
-    res.status(400).json({ msg: 'We need trainer name, trainer surname, phone, activityId, salary and status' });
+    res.status(400).json({ msg: 'We need trainer name, trainer surname,
+     phone, activityId, salary and status' });
   } else {
     const trainerIdentification = trainers[trainers.length - 1].id + 1;
     const newTrainer = {
@@ -49,49 +53,8 @@ router.post('/', (req, res) => {
       }
     });
   }
-});
+}); */
 
-router.put('/:id', (req, res) => {
-  const trainerId = req.params.id;
-  const updateTrainer = req.body;
-  const foundTrainer = trainers.find((trainer) => trainer.id.toString() === trainerId);
-  if (foundTrainer) {
-    foundTrainer.firstName = updateTrainer.firstName ? updateTrainer.firstName
-      : foundTrainer.firstName;
-    foundTrainer.lastName = updateTrainer.lastName ? updateTrainer.lastName : foundTrainer.lastName;
-    foundTrainer.phone = updateTrainer.phone ? updateTrainer.phone : foundTrainer.phone;
-    foundTrainer.activityId = updateTrainer.activityId ? updateTrainer.activityId
-      : foundTrainer.activityId;
-    foundTrainer.salary = updateTrainer.salary ? updateTrainer.salary : foundTrainer.salary;
-    foundTrainer.status = updateTrainer.status ? updateTrainer.status : foundTrainer.status;
-    const trainer = trainers.filter((train) => train.id.toString() !== trainerId);
-    trainer.push(foundTrainer);
-    fs.writeFile('src/data/trainer.json', JSON.stringify(trainers, null, 2), (err) => {
-      if (err) {
-        res.status(400).json({ msg: 'Error! Trainer can not be update' });
-      } else {
-        res.status(200).json({ msg: 'Trainer updated!', data: foundTrainer });
-      }
-    });
-  } else {
-    res.status(400).json({ msg: `No trainer with the id of ${req.params.id}` });
-  }
-});
-
-router.delete('/:id', (req, res) => {
-  const trainerId = req.params.id;
-  const filterTrainer = trainers.filter((trainer) => trainer.id.toString() !== trainerId);
-  if (filterTrainer.length === trainers.length) {
-    res.status(400).json({ msg: 'Trainer not found!' });
-  } else {
-    fs.writeFile('src/data/trainer.json', JSON.stringify(filterTrainer, null, 2), (err) => {
-      if (err) {
-        res.status(400).json({ msg: 'Error! Trainer can not be deleted' });
-      } else {
-        res.status(200).json({ msg: 'Trainer deleted!' });
-      }
-    });
-  }
-});
-
-module.exports = router;
+module.exports = {
+  getAllTrainers,
+};
---
+++ src/routes/trainers.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const trainerController = require('../controllers/trainers');
+const validation = require('../validations/trainers');
+
+const router = express.Router();
+
+router
+  .get('/', trainerController.getAllTrainers)
+  .get('/:id', trainerController.getTrainerById)
+  .post('/', validation.validateCreation, trainerController.createTrainer);
+
+module.exports = router;
