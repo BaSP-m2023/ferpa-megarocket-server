f2abd7e824358f92cfa34a112141596d257567d1
RodrigoF <r.fontan12@gmail.com>
2023-05-13 19:09:21 -0300

    MR-04: Add get by id and post method in subscriptions

--- src/controllers/subscriptions.js
+++ src/controllers/subscriptions.js
@@ -13,6 +13,38 @@ const getAllSub = (req, res) => {
     }));
 };
 
+const getSubById = (req, res) => {
+  const { id } = req.params;
+
+  Subscription.findById(id)
+    .then((subscriptions) => res.status(200).json({
+      message: 'Subscription found!',
+      data: subscriptions,
+      error: false,
+    }))
+    .catch((error) => res.status(500).json({
+      message: 'An error ocurred',
+      error,
+    }));
+};
+
+const createSub = (req, res) => {
+  const { _class, member, date } = req.body;
+
+  Subscription.create({
+    _class,
+    member,
+    date,
+  })
+    .then((result) => res.status(201).json(result))
+    .catch((error) => res.status(400).json({
+      message: 'An error ocurred',
+      error,
+    }));
+};
+
 module.exports = {
   getAllSub,
+  getSubById,
+  createSub,
 };
