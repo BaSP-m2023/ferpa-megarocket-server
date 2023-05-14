29846b9a059e6bdecde766991241fcd7e49cd3e1
Nicolas Kler <nicolasjosekler@gmail.com>
2023-05-14 20:57:52 -0300

    MR-02: Add method delete

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -15,6 +15,27 @@ const getMemberById = (req, res) => {
     }));
 };
 
+const deleteMember = (req, res) => {
+  const { id } = req.params;
+  Member.findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          message: `Member with id: ${id} was not found`,
+          error: true,
+        });
+      }
+      return res.status(200).json({
+        message: `Member with id: ${id} was deteled`,
+      });
+    })
+    .catch((error) => res.status(400).json({
+      message: 'An error ocurred!',
+      error,
+    }));
+};
+
 module.exports = {
   getMemberById,
+  deleteMember,
 };
