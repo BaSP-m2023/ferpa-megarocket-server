fa0ef8a02dae9b8b865095cdbed901ea1d7f5cca
Victoria Negrotto <vnegrotto19@gmail.com>
2023-05-15 16:12:37 -0300

    MR-14: fixed id validation

--- src/controllers/classes.js
+++ src/controllers/classes.js
@@ -1,30 +1,38 @@
 const Class = require('../models/Class');
 
+const regexObjectId = /^[0-9a-fA-F]{24}$/;
+
 const getAllClasses = (req, res) => {
   Class.find()
     .then((classes) => res.status(200).json({
-      messege: 'Class list',
+      message: 'Class list',
       data: classes,
       error: false,
     }))
     .catch((error) => res.status(400).json({
-      messege: 'Error',
+      message: 'Error',
       error,
     }));
 };
 
 const getClassById = (req, res) => {
   const { id } = req.params;
-  const regexObjectId = /^[0-9a-fA-F]{24}$/;
+  if (!id.match(regexObjectId)) {
+    res.status(400).json({
+      message: 'ID invalid, please correct',
+      data: undefined,
+      error: true,
+    });
+  }
 
-  Class.findById(id, regexObjectId)
+  Class.findById(id)
     .then((classes) => res.status(200).json({
       messege: `Class found! It was ${classes.activityId}`,
       data: classes,
       error: false,
     }))
     .catch((error) => res.json({
-      messsege: "Error, Class don't found",
+      message: "Error, Class don't found",
       data: undefined,
       error,
     }));
@@ -43,12 +51,12 @@ const createClass = (req, res) => {
     slots,
   })
     .then((classes) => res.status(200).json({
-      messege: 'Class created!',
+      message: 'Class created!',
       data: classes,
       error: false,
     }))
     .catch((error) => res.status(400).json({
-      messege: "Error, don't created",
+      message: "Error, don't created",
       data: undefined,
       error,
     }));
