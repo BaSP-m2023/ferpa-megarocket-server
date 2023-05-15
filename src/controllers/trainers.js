38e63bbb91f36e93325ebb34c04db669550212ac
LucianoStradiot <lstradiot15@gmail.com>
2023-05-15 13:05:15 -0300

    MR-3: Fix the vercel problem

--- src/controllers/trainers.js
+++ src/controllers/trainers.js
@@ -3,12 +3,12 @@ const Trainer = require('../models/Trainer');
 const getAllTrainers = (req, res) => {
   Trainer.find()
     .then((trainers) => res.status(200).json({
-      message: 'Trainers list',
+      message: 'Trainers list.',
       data: trainers,
       error: false,
     }))
     .catch((error) => res.status(500).json({
-      message: 'An error ocurred',
+      message: 'An error ocurred.',
       error,
     }));
 };
@@ -20,19 +20,19 @@ const getTrainerById = (req, res) => {
     .then((trainer) => {
       if (!trainer) {
         res.status(404).json({
-          message: `Trainers with the id ${id} was not found`,
+          message: `Trainers with the id ${id} was not found.`,
           error: true,
         });
       } else {
         res.status(200).json({
-          message: `Trainers with the id ${id} was succesfully found`,
+          message: `Trainers with the id ${id} was succesfully found.`,
           data: trainer,
           error: false,
         });
       }
     })
     .catch((error) => res.json({
-      message: 'An error ocurred',
+      message: 'An error ocurred.',
       error,
     }));
 };
@@ -53,12 +53,12 @@ const createTrainer = (req, res) => {
     salary,
   })
     .then((trainer) => res.status(201).json({
-      message: 'Trainer was succesfully created',
+      message: 'Trainer was succesfully created.',
       data: trainer,
       error: false,
     }))
     .catch((error) => res.status(400).json({
-      message: 'An error ocurred',
+      message: 'An error ocurred.',
       error,
     }));
 };
