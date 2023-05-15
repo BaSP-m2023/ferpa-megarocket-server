80a2b08abdc240d796a6657d655f8e848a3eb737
Galo Durante <durantegalo@gmail.com>
2023-05-15 14:22:51 -0300

    MR-15: add validation on methods

--- src/controllers/trainers.js
+++ src/controllers/trainers.js
@@ -1,64 +1,78 @@
 const Trainer = require('../models/Trainer');
 
+const regexObjectId = /^[0-9a-fA-F]{24}$/;
+
 const deleteTrainer = (req, res) => {
   const { id } = req.params;
-  Trainer.findByIdAndDelete(id)
-    .then((result) => {
-      if (!result) {
-        return res.status(404).json({
-          message: `Trainer with id ${id} was not found`,
-          error: true,
+  if (!id.match(regexObjectId)) {
+    res.status(400).json({
+      message: 'Id invalid, try again!',
+    });
+  } else {
+    Trainer.findByIdAndDelete(id)
+      .then((result) => {
+        if (!result) {
+          return res.status(404).json({
+            message: `Trainer with id ${id} was not found`,
+            error: true,
+          });
+        }
+        return res.status(200).json({
+          message: `Trainer with id: ${id} was succesfully deleted.`,
+          error: false,
         });
-      }
-      return res.status(200).json({
-        message: `Trainer with id: ${id} was succesfully deleted.`,
-        error: false,
-      });
-    })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred!',
-      error,
-    }));
+      })
+      .catch((error) => res.status(500).json({
+        message: 'An error ocurred!',
+        error,
+      }));
+  }
 };
 
 const updateTrainer = (req, res) => {
   const { id } = req.params;
-  const {
-    firstName, lastName, dni, phone, email, city, password, salary, isActive,
-  } = req.body;
+  if (!id.match(regexObjectId)) {
+    res.status(400).json({
+      message: 'Id invalid, try again!',
+    });
+  } else {
+    const {
+      firstName, lastName, dni, phone, email, city, password, salary, isActive,
+    } = req.body;
 
-  Trainer.findByIdAndUpdate(
-    id,
-    {
-      firstName,
-      lastName,
-      dni,
-      phone,
-      email,
-      city,
-      password,
-      salary,
-      isActive,
-    },
-    { new: true },
-  )
-    .then((result) => {
-      if (!result) {
-        return res.status(404).json({
-          message: `Trainer with id ${id} was not found`,
-          error: true,
+    Trainer.findByIdAndUpdate(
+      id,
+      {
+        firstName,
+        lastName,
+        dni,
+        phone,
+        email,
+        city,
+        password,
+        salary,
+        isActive,
+      },
+      { new: true },
+    )
+      .then((result) => {
+        if (!result) {
+          return res.status(404).json({
+            message: `Trainer with id ${id} was not found`,
+            error: true,
+          });
+        }
+        return res.status(200).json({
+          message: `Trainer with id: ${id} was succesfully updated`,
+          data: result,
+          error: false,
         });
-      }
-      return res.status(200).json({
-        message: `Trainer with id: ${id} was succesfully updated`,
-        data: result,
-        error: false,
-      });
-    })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred!',
-      error,
-    }));
+      })
+      .catch((error) => res.status(500).json({
+        message: 'An error ocurred!',
+        error,
+      }));
+  }
 };
 
 module.exports = {
