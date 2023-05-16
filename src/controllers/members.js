ff1ac0ffbe08e87ce357e2737863d7234d78c8ff
Nicolas Kler <nicolasjosekler@gmail.com>
2023-05-15 22:46:25 -0300

    MR-02: Add validation prior to find id

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -1,45 +1,71 @@
 const Member = require('../models/Member');
 
+const regexObjectId = /^[0-9a-fA-F]{24}$/;
+
 const getMemberById = (req, res) => {
   const { id } = req.params;
+  if (!id.match(regexObjectId)) {
+    res.status(400).json({
+      message: `Id:${id} is invalid`,
+      data: undefined,
+      error: true,
+    });
+  }
   Member.findById(id)
     .then((member) => {
       if (!member) {
-        return res.status(404).json({
-          message: `Member with id: ${id} was not found`,
+        res.status(404).json({
+          message: `Member with id: ${id} was not found.`,
+          data: undefined,
           error: true,
         });
       }
-      return res.status(200).json({
-        message: `Member found! It was ${id}`,
+      res.status(200).json({
+        message: `Member with id: ${id} was found.`,
         data: member,
+        error: false,
       });
     })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred!',
-      error,
-    }));
+    .catch((error) => {
+      res.status(500).json({
+        message: 'An error ocurred',
+        data: undefined,
+        error,
+      });
+    });
 };
 
 const deleteMember = (req, res) => {
   const { id } = req.params;
+  if (!id.match(regexObjectId)) {
+    res.status(400).json({
+      message: `Id:${id} is invalid`,
+      data: undefined,
+      error: true,
+    });
+  }
   Member.findByIdAndDelete(id)
-    .then((result) => {
-      if (!result) {
-        return res.status(404).json({
-          message: `Member with id: ${id} was not found`,
+    .then((member) => {
+      if (!member) {
+        res.status(404).json({
+          message: `Member with id: ${id} was not found.`,
+          data: undefined,
           error: true,
         });
       }
-      return res.status(200).json({
-        message: `Member with id: ${id} was deteled`,
+      res.status(200).json({
+        message: `Member with id: ${id} was removed.`,
+        data: member,
         error: false,
       });
     })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred!',
-      error,
-    }));
+    .catch((error) => {
+      res.status(500).json({
+        message: 'An error ocurred',
+        data: undefined,
+        error,
+      });
+    });
 };
 
 module.exports = {
