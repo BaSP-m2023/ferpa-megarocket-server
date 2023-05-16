2ff360e957127746a15bb0c7346b672096fcc57c
Victoria Negrotto <vnegrotto19@gmail.com>
2023-05-15 23:45:38 -0300

    MR-14: fixed schema

--- src/models/Class.js
+++ src/models/Class.js
@@ -6,19 +6,25 @@ const ClassSchema = new Schema({
   day: {
     type: String,
     enum: ['Monday', 'Wednesday', 'Friday'],
+    require: true,
   },
   hour: {
     type: Number,
     timestamps: true,
+    require: true,
+
   },
   trainerId: {
     type: String,
+    require: true,
   },
   activityId: {
     type: String,
+    require: true,
   },
   slots: {
     type: Number,
+    require: true,
   },
 });
 
