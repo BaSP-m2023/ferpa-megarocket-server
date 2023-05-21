09d154eed7762a84f58bfa11894f9cffa80b91d0
LucianoStradiot <lstradiot15@gmail.com>
2023-05-21 18:37:47 -0300

    MR-21: Add the test for the get methods

---
+++ src/seeds/trainers.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('64667332ecb50c522415bea5'),
+    firstName: 'lucho',
+    lastName: 'alvarez',
+    dni: 37966691,
+    phone: 11457345909,
+    email: 'araña@gmail.co',
+    password: 'askda231.',
+    salary: 2355634,
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667332ecb50c522415bea3'),
+    firstName: 'lucho',
+    lastName: 'stradiot',
+    dni: 37930691,
+    phone: 1145768909,
+    email: 'lucho@gmail.co',
+    password: 'askda231.',
+    salary: 2355634,
+    isActive: false,
+  },
+];
--- src/tests/trainers.test.js
+++ src/tests/trainers.test.js
@@ -1,3 +1,34 @@
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import trainersSeed from '../seeds/trainers';
+import Trainer from '../models/Trainer';
+
+beforeAll(async () => {
+  await Trainer.collection.insertMany(trainersSeed);
+});
+
+describe('GET/api/trainers', () => {
+  test('succesfull 200 response', async () => {
+    const res = await request(app).get('/api/trainers').send();
+    expect(res.status).toBe(200);
+    expect(res.error).toBeFalsy();
+  });
+  test('Should get a 404 response', async () => {
+    const res = await request(app).get('/api/trainer').send();
+    expect(res.status).toBe(404);
+    expect(res.error).toBeTruthy();
+  });
+});
+
+describe('GET/api/trainers/:id', () => {
+  test('succesfull 200 response', async () => {
+    const res = await request(app).get('/api/trainers/64667332ecb50c522415bea5').send();
+    expect(res.status).toBe(200);
+    expect(res.error).toBeFalsy();
+  });
+  test('Should get a 404 response', async () => {
+    const res = await request(app).get('/api/trainer/64667332ecb50c522415bea2').send();
+    expect(res.status).toBe(404);
+    expect(res.error).toBeTruthy();
+  });
 });
