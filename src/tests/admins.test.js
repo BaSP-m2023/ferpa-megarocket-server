436e7d3d4f9fae677142d93a99e5cb85a3186c92
Eugenia Gassman <eugeniagassman@gmail.com>
2023-05-21 20:39:53 -0300

    MR-28: :test of get and get by id methods

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -1,3 +1,56 @@
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import adminsSeed from '../seeds/admins';
+import Admins from '../models/Admin';
+
+// const updAdmin = {
+//   firstName: 'Euge',
+//   lastName: 'Admin',
+//   dni: '11111111',
+//   phone: '22222222',
+//   email: 'admin@admin.com',
+//   city: 'Rosario',
+//   password: 'canelones1234!',
+// };
+
+beforeAll(async () => {
+  await Admins.collection.insertMany(adminsSeed);
 });
+
+describe('GET/api/admins', () => {
+  test('should get all admins', async () => {
+    const res = await request(app).get('/api/admins').send();
+    expect(res.status).toBe(200);
+    expect(res.error).toBeFalsy();
+  });
+  test('should throw a wrong path error and status 404', async () => {
+    const res = await request(app).get('/api/admin').send();
+    expect(res.status).toBe(404);
+    expect(res.error).toBeTruthy();
+  });
+  test('should get one admin by id', async () => {
+    const res = await request(app).get('/api/admins/64654f260425c5c72ff190fe').send();
+    // eslint-disable-next-line no-underscore-dangle
+    expect(res.body.data._id).toEqual('64654f260425c5c72ff190fe');
+    expect(res.status).toBe(200);
+    expect(res.error).toBeFalsy();
+  });
+  test('should get an error for invalid id', async () => {
+    const res = await request(app).get('/api/admins/64654f260425').send();
+    expect(res.status).toBe(400);
+    expect(res.error).toBeTruthy();
+  });
+  test('must throw an error for non-existent id', async () => {
+    const res = await request(app).get('/api/admins/64654f260425c5c72ff190fa').send();
+    expect(res.status).toBe(404);
+    expect(res.error).toBeTruthy();
+  });
+});
+
+// describe('PUT/api/admins/:id', () => {
+//   test('should update an admin by id', async () => {
+//     const res = await request(app).put('/api/admins/646550c10425c5c72ff19102').send(updAdmin);
+//     expect(res.status).toBe(200);
+//     expect(res.body.error).toBeFalsy();
+//   });
+// });
