c6ae0659f014762b19060962c29c6d26d5aad8b1
FontanR <r.fontan12@gmail.com>
2023-05-23 13:19:56 -0300

    MR-24: Add delete and put tests

--- src/seeds/subscriptions.js
+++ src/seeds/subscriptions.js
@@ -1,18 +1,26 @@
 import mongoose from 'mongoose';
 
+const currentDate = new Date();
+
+const outOfWeekDate = new Date(
+  currentDate.getFullYear(),
+  currentDate.getMonth(),
+  currentDate.getDate() - currentDate.getDay() + 14,
+);
+
 export default [
   {
     _id: new mongoose.Types.ObjectId('64693ee0ed79af5a83aac57c'),
     classId: new mongoose.Types.ObjectId('6462d8c5afd4e4d023690d66'),
     memberId: new mongoose.Types.ObjectId('6462439ab74486265babed70'),
-    date: '2023-03-11T00:00:00.000Z',
+    date: new Date(),
     __v: 0,
   },
   {
     _id: new mongoose.Types.ObjectId('646936a8c657bcbd138eaf3c'),
     classId: new mongoose.Types.ObjectId('6462d8c5afd4e4d023690d65'),
     memberId: new mongoose.Types.ObjectId('646243a5b74486265babed72'),
-    date: '2023-05-17T00:00:00.000+00:00',
+    date: outOfWeekDate,
     __v: 3,
   },
 ];
--- src/tests/subscriptions.test.js
+++ src/tests/subscriptions.test.js
@@ -9,16 +9,25 @@ import subSeed from '../seeds/subscriptions';
 import memberSeed from '../seeds/members';
 import classSeed from '../seeds/classes';
 
+let subId;
+
+const currentDate = new Date();
+const outOfWeekDate = new Date(
+  currentDate.getFullYear(),
+  currentDate.getMonth(),
+  currentDate.getDate() - currentDate.getDay() - 14,
+);
+
 const mockSubscription = {
   classId: '6462d8c5afd4e4d023690d66',
   memberId: '646243a5b74486265babed72',
-  date: '2023-05-21',
+  date: new Date(),
 };
 
 const mockSubscription2 = {
   classId: '6462d8c5afd4e4d023690d66',
   memberId: '646243a5b74486265babed72',
-  date: '2023-05-25',
+  date: outOfWeekDate,
 };
 
 beforeAll(async () => {
@@ -27,23 +36,23 @@ beforeAll(async () => {
   await Class.collection.insertMany(classSeed);
 });
 
-describe('GET /api/subscriptions', () => {
-  test('GET ALL', async () => {
+describe('GET THIS WEEK SUB /api/subscriptions', () => {
+  test('GET THIS WEEK SUB: should return 1 subscription in data', async () => {
     const response = await request(app).get('/api/subscriptions/').send();
-    console.log(response.body.data);
     expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(1);
   });
 });
 
 describe('GET /api/subscriptions', () => {
-  test('GET ALL', async () => {
+  test('GET: should return 2 subscriptions in data', async () => {
     const response = await request(app).get('/api/subscriptions/all').send();
-    expect(response.body.data.length).toBe(2);
     expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(2);
   });
   test('GET BY ID: should return the name Juan from the selected subscription', async () => {
     const response = await request(app).get('/api/subscriptions/646936a8c657bcbd138eaf3c').send();
-    expect(response.body.data.memberId.firstName).toBe('Juan');
+    expect(response.body.data.memberId.dni).toBe('362355133456');
   });
 });
 
@@ -57,12 +66,38 @@ describe('POST /api/subscriptions', () => {
     const seedPostLength = response.body.data.length;
     expect(seedLength + 1).toBe(seedPostLength);
   });
-  test('POST: should return the last name Martinez from the selected subscription', async () => {
+  test('POST: should return the dni: 362355133456 from the member of the new subscription', async () => {
     let response = await request(app).post('/api/subscriptions/').send(mockSubscription2);
+    // eslint-disable-next-line no-underscore-dangle
     expect(response.status).toBe(201);
     // eslint-disable-next-line no-underscore-dangle
-    const subId = response.body.data._id;
+    subId = response.body.data._id;
     response = await request(app).get(`/api/subscriptions/${subId}`).send();
-    expect(response.body.data.memberId.lastName).toBe('Martinez');
+    expect(response.body.data.memberId.dni).toBe('362355133456');
+  });
+});
+
+describe('DELETE /api/subscriptions', () => {
+  test('DELETE: the length of the data should decrease by 1 unit', async () => {
+    let response = await request(app).get('/api/subscriptions/all').send();
+    const seedLength = response.body.data.length;
+    response = await request(app).delete(`/api/subscriptions/${subId}`).send();
+    expect(response.status).toBe(200);
+    response = await request(app).get('/api/subscriptions/all').send();
+    const seedDeleteLength = response.body.data.length;
+    expect(seedLength - 1).toBe(seedDeleteLength);
+  });
+});
+
+describe('PUT /api/subscriptions', () => {
+  test('PUT: should check member dni and data length should be the same', async () => {
+    let response = await request(app).get('/api/subscriptions/all').send();
+    const seedLength = response.body.data.length;
+    response = await request(app).get('/api/subscriptions/646936a8c657bcbd138eaf3c').send(mockSubscription2);
+    expect(response.status).toBe(200);
+    expect(response.body.data.memberId.dni).toBe('362355133456');
+    response = await request(app).get('/api/subscriptions/all').send();
+    const seedPutLength = response.body.data.length;
+    expect(seedLength).toBe(seedPutLength);
   });
 });
