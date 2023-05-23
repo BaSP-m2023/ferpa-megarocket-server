7f89bc4f43bc27065e3ebd4be212bd2170559ee0
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2023-05-23 09:11:01 -0300

    MR-19: Added test for DELETE and modified the test for POST

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -25,9 +25,45 @@ describe('POST /api/admins', () => {
       .send(mockAdmin);
     expect(response.statusCode).toBe(201);
   });
-  test('POST: Should have a valid password', async () => {
-    const response = await request(app).post('/api/admins').send(mockAdmin);
+  test('POST: Admin should have a valid password', async () => {
+    const response = await request(app)
+      .post('/api/admins')
+      .send(mockAdmin);
     const admin = response.body.data;
     expect(admin.password).toMatch(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+  });
+  test('POST: A new admin should be add to the database', async () => {
+    let response = await request(app)
+      .post('/api/admins')
+      .send(mockAdmin);
+    // eslint-disable-next-line no-underscore-dangle
+    const newAdminId = response.body.data._id;
+    response = await request(app)
+      // eslint-disable-next-line no-underscore-dangle
+      .get(`/api/admins/${newAdminId}`);
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe('DELETE /api/admins:id', () => {
+  test('DELETE: Should return a status 400 when this route is hit', async () => {
+    const response = await request(app)
+      .delete('/api/admins/64654');
+    expect(response.statusCode).toBe(400);
+  });
+  test('DELETE: Should return a status 404 when this route is hit', async () => {
+    const response = await request(app)
+      .delete('/api/admins/64654f260425c5c72ff190fd');
+    expect(response.statusCode).toBe(404);
+  });
+  test('DELETE: Should delete one admin from the database', async () => {
+    let response = await request(app)
+      .get('/api/admins/64654f260425c5c72ff190fe');
+    expect(response.statusCode).toBe(200);
+    response = await request(app)
+      .delete('/api/admins/64654f260425c5c72ff190fe');
+    response = await request(app)
+      .get('/api/admins/64654f260425c5c72ff190fe');
+    expect(response.statusCode).toBe(404);
   });
 });
