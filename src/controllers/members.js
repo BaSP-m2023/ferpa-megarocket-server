d9583bdb80f33ef98cf2e384a293db96add0456d
leo-balonchard <leobalonchard@gmail.com>
2023-05-23 14:55:34 -0300

    MR-27: modification of member controller

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -1,7 +1,6 @@
 const Member = require('../models/Member');
 
 const regexObjectId = /^[0-9a-fA-F]{24}$/;
-
 const getAllMembers = (req, res) => {
   Member.find()
     .then((members) => res.status(200).json({
@@ -14,12 +13,10 @@ const getAllMembers = (req, res) => {
       error,
     }));
 };
-
 const createMember = (req, res) => {
   const {
     firstName, lastName, dni, phone, email, city, birthDay, postalCode, isActive, membership,
   } = req.body;
-
   Member.create({
     firstName,
     lastName,
@@ -42,11 +39,10 @@ const createMember = (req, res) => {
       error,
     }));
 };
-
 const updateMember = (req, res) => {
   const { id } = req.params;
   if (!id.match(regexObjectId)) {
-    res.status(400).json({
+    return res.status(400).json({
       message: 'Id invalid, try again!',
       data: undefined,
       error: true,
@@ -55,7 +51,6 @@ const updateMember = (req, res) => {
     const {
       firstName, lastName, dni, phone, email, city, birthDay, postalCode, isActive, membership,
     } = req.body;
-
     Member.findByIdAndUpdate(
       id,
       {
@@ -88,11 +83,12 @@ const updateMember = (req, res) => {
       })
       .catch((error) => res.status(500).json({
         message: 'An error ocurred!',
+        data: undefined,
         error,
       }));
   }
+  return false;
 };
-
 const getMemberById = (req, res) => {
   const { id } = req.params;
   if (!id.match(regexObjectId)) {
@@ -117,16 +113,13 @@ const getMemberById = (req, res) => {
         error: false,
       });
     })
-    .catch((error) => {
-      res.status(500).json({
-        message: 'An error ocurred',
-        data: undefined,
-        error,
-      });
-    });
+    .catch((error) => res.status(500).json({
+      message: 'An error ocurred!',
+      data: undefined,
+      error,
+    }));
   return false;
 };
-
 const deleteMember = (req, res) => {
   const { id } = req.params;
   if (!id.match(regexObjectId)) {
@@ -160,7 +153,6 @@ const deleteMember = (req, res) => {
     });
   return false;
 };
-
 module.exports = {
   getAllMembers,
   createMember,
