6701571685c1bf624d6f0ad3bfb3d2a7e63ce4ad
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2023-05-22 23:58:22 -0300

    MR-19: Added seed file and test for post method

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -1,3 +1,33 @@
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+
+import Admin from '../models/Admin';
+import adminSeed from '../seeds/admins';
+
+const mockAdmin = {
+  firstName: 'Fer',
+  lastName: 'Admin',
+  dni: '12345678',
+  phone: '123457890',
+  email: 'admin@gmail.com',
+  city: 'Rosario',
+  password: 'canelones1234!',
+};
+
+beforeAll(async () => {
+  await Admin.collection.insertMany(adminSeed);
+});
+
+describe('POST /api/admins', () => {
+  test('POST: Should return a status 201', async () => {
+    const response = await request(app)
+      .post('/api/admins')
+      .send(mockAdmin);
+    expect(response.statusCode).toBe(201);
+  });
+  test('POST: Should have a valid password', async () => {
+    const response = await request(app).post('/api/admins').send(mockAdmin);
+    const admin = response.body.data;
+    expect(admin.password).toMatch(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+  });
 });
