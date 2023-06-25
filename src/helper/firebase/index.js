bcb0c2b173129acdd0bafe94a12da872512578a3
Daniel Lezama <dlezama0796@gmail.com>
2023-06-25 17:02:48 -0300

    MR-132: second try

--- src/helper/firebase/index.js
+++ src/helper/firebase/index.js
@@ -1,14 +1,14 @@
 import * as admin from 'firebase-admin';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
-const privateKey = process.env.PRIVATE_KEY_FIREBASE.replace(/\\n/g, '\n');
 const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert({
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: privateKey,
+    private_key: process.env.FIREBASE_PROJECT_ID.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
