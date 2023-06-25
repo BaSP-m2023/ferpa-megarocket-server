a8ea6433b39a7e4cad6acd3d7f2f53d68c28f1cc
Daniel Lezama <dlezama0796@gmail.com>
2023-06-25 18:46:14 -0300

    MR-132: third try

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import app from './app';
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
+/* mongoose.connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
+  .then(() => console.log('🟢 DB Connected!'))
+  .then(() => app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}!`)))
+  .catch((error) => console.log('🔴 There was an error on the DB connection method.', error)); */
+
+/* mongoose.connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
   .then(() => console.log('MongoDB connected'))
   .catch((e) => console.log(e));
 
@@ -21,3 +26,27 @@ try {
 } catch (error) {
   console.log('There was an error starting the server: ', error);
 }
+ */
+
+mongoose.connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('MongoDB connected');
+
+    try {
+      app.listen(process.env.PORT, (err) => {
+        if (err) throw err;
+        // eslint-disable-next-line no-console
+        console.log(`server listening on port: ${process.env.PORT}`);
+      });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log('There was an error starting the server: ', err);
+    }
+  })
+// eslint-disable-next-line no-console
+  .catch((e) => console.log(e));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
